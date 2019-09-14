0f076b54cd3e4cec99a1404f69c92495db59ac35
parkJungWoong <myuoong@gmail.com>
2019-09-15 01:00:10 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# python-study
-파이썬 공부
+# 파이썬 공부
+
+## 참고 사이트
+- [문법 정리](https://wikidocs.net/book/1) 
---
+++ syntax/string.py
@@ -0,0 +1,18 @@
+#문자열 연산
+a = "one"
+b = "-two"
+print(a + b)
+
+temp='''
+who
+are
+you
+'''
+print(temp)
+
+mulistr='python'
+print(mulistr*10)
+print(len(mulistr))
+print(mulistr[-3])
+print(mulistr[4])
+print(mulistr[0:4])
